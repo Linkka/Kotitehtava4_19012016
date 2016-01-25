8f5ac8547fdcf53d653efb36bdb6864b9952f710
Linkka <samilindroos@hotmail.com>
2016-01-26 00:41:05 +0200

    Inital commit.

---
+++ Kotitehtava4olio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava4olio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a one vehicle instance
+            Vehicle volvo = new Vehicle();
+            volvo.Name = "Volvo S60";
+            volvo.Speed = 130;
+            volvo.Tyres = 4;
+            // display vehicle data
+            volvo.PrintData();
+            Console.ReadLine();
+            // return all vehicle's values as a single string.
+            volvo.ToString();
+
+            // accelerate, change car's speed value
+            volvo.Accelerate();
+            // display vehicle data again.
+            volvo.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kotitehtava4olio/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava4olio
+{
+    class Vehicle
+    {
+        // properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        // don't create any constructor, so default one will be used
+
+
+        // method to give more speed
+        public void Accelerate()
+        {
+            Speed += 5;
+        }
+
+        // method to display vehicle data
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle data : ");
+            Console.WriteLine("- Name : " + Name);
+            Console.WriteLine("- Speed : " + Speed);
+            Console.WriteLine("- Tyres : " + Tyres);
+        }
+
+        // method to give more speed
+        public string ToString(string Name, int Speed, int Tyres)
+        {
+            return (Name + Speed + Tyres);
+        }
+
+        // destructor
+        ~Vehicle()
+        {
+            Console.WriteLine("Vehicle object destroyed.");
+        }
+
+    }
+
+}
